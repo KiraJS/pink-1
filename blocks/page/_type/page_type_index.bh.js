daf12a8b4b15812afdca7658f6252c16247313ec
Alexander Polovnikov <aleksandrpol@me.com>
2014-12-15 03:02:14 +0300

    fixup! Планшетная версия Pink

--- blocks/page/_type/page_type_index.bh.js
+++ blocks/page/_type/page_type_index.bh.js
@@ -1,7 +1,6 @@
 module.exports = function(bh) {
     bh.match('page_type_index', function(ctx) {
         ctx.content([
-            // { block: 'header' },
             { block: 'intro' },
             { block: 'os' },
             { block: 'features', elem: 'header' },
